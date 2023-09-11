c3380ae848af15ddc16f259931a8674767267969
aleena6875 <aleenashibu48@gmail.com>
2023-09-11 19:59:39 +0530

    Second commit

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.musicapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button=findViewById(R.id.button)
+        button.setOnClickListener {
+            val i= Intent(this,MainActivity2::class.java)
+            startActivity(i)
+        }
     }
 }
--- app/src/main/java/com/example/musicapp/MainActivity2.kt
+++ app/src/main/java/com/example/musicapp/MainActivity2.kt
@@ -1,11 +1,19 @@
 package com.example.musicapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        button=findViewById(R.id.button)
+        button.setOnClickListener {
+            val i= Intent(this,MainActivity3::class.java)
+            startActivity(i)
+        }
     }
 }
