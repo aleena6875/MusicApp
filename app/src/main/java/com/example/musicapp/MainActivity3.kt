7453cecd2e21a905e94b9897ab62f6b8fd317500
aleena6875 <aleenashibu48@gmail.com>
2023-09-13 18:45:50 +0530

    code changes

--- app/src/main/java/com/example/musicapp/MainActivity3.kt
+++ app/src/main/java/com/example/musicapp/MainActivity3.kt
@@ -3,13 +3,19 @@ package com.example.musicapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ProgressBar
+import androidx.appcompat.app.ActionBar
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.progressindicator.LinearProgressIndicator
 
 class MainActivity3 : AppCompatActivity() {
     private lateinit var bottomNav:BottomNavigationView
+    private lateinit var progressBar: LinearProgressIndicator
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
@@ -41,38 +47,11 @@ class MainActivity3 : AppCompatActivity() {
         }
 
         }
-
+        progressBar=findViewById(R.id.progressBar)
+        Handler().postDelayed({
+            progressBar.visibility = View.INVISIBLE
+        },3000)
     }
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        menuInflater.inflate(R.menu.bottom_navigation_menu, menu)
-//        return true
-//    }
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when (item.itemId) {
-//            R.id.item1 -> {
-//                val intent = Intent(this, MainActivity3::class.java)
-//                startActivity(intent)
-//                return true
-//            }
-//            R.id.item2 -> {
-//                val intent = Intent(this, MainActivity4::class.java)
-//                startActivity(intent)
-//                return true
-//            }
-//            R.id.item3 -> {
-//                val intent = Intent(this, MainActivity3::class.java)
-//                startActivity(intent)
-//                return true
-//            }
-//            R.id.item4 -> {
-//                val intent = Intent(this, MainActivity3::class.java)
-//                startActivity(intent)
-//                return true
-//            }
-//            // Add more menu items as needed
-//            else -> return super.onOptionsItemSelected(item)
-//        }
-//    }
 
 
 }
